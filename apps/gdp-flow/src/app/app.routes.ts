af189b3c3c3f196c76b978749c7486eb312e9ed1
thaua-engelmann <engelmann.webdev@gmail.com>
2024-11-23 02:15:51 -0300

    feat(perfil): criado página de perfil e ajustado rota

--- apps/gdp-flow/src/app/app.routes.ts
+++ apps/gdp-flow/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Route } from '@angular/router';
+import { ProfileComponent } from '../app/pages/profile/profile.component';
 
 export const appRoutes: Route[] = [
     {
         path: '',
         pathMatch: 'full',
         loadComponent: () => import('../app/pages/sign-up/sign-up.component').then(c => c.SignUpComponent)
+    },
+    {
+        path: 'perfil',
+        loadComponent: () => import('../app/pages/profile/profile.component').then(c => ProfileComponent)
     }
 ];
