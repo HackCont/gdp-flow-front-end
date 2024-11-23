26e62887cc8a57c40a20f62b770e8ae8fadee669
rafaelmjardim <rafaelmjardim@hotmail.com>
2024-11-23 17:13:21 -0300

    feat(router-header): adicionado componente de router header

---
+++ apps/gdp-flow/src/app/ui/router-header/router-header.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-router-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './router-header.component.html',
+  styleUrl: './router-header.component.scss',
+})
+export class RouterHeaderComponent {
+  @Input() text!: string;
+}
