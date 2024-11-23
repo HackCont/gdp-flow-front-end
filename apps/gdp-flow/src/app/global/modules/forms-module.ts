c641a97acb9398f4b4ec05391bf0d4cb16894516
thaua-engelmann <engelmann.webdev@gmail.com>
2024-11-23 11:39:34 -0300

    feat(shared): alimentado shared styles e criado arquivo de shared para os módulos do formulário

---
+++ apps/gdp-flow/src/app/global/modules/forms-module.ts
@@ -0,0 +1,5 @@
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+
+export const FORMS_MODULE = [ReactiveFormsModule, MatFormFieldModule, MatInputModule]
