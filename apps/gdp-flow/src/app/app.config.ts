95eadf02ca68088eb6b10f1f8625ff8b23d95dee
thaua-engelmann <engelmann.webdev@gmail.com>
2024-11-23 11:38:00 -0300

    feat(sign-up): criado formulário página de sign-up

--- apps/gdp-flow/src/app/app.config.ts
+++ apps/gdp-flow/src/app/app.config.ts
@@ -3,12 +3,15 @@ import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
-    provideAnimations()
+    provideAnimations(),
+
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}}
   ],
 };
--- apps/gdp-flow/src/app/pages/sign-up/sign-up.component.ts
+++ apps/gdp-flow/src/app/pages/sign-up/sign-up.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthTemplateComponent } from "../../ui/auth-template/auth-template.component";
+import { FORMS_MODULE } from '../../global/modules/forms-module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EMAIL_VALIDATION, PASSWORD_VALIDATION } from '../../global/validations/auth-validations';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AuthTemplateComponent, ...FORMS_MODULE],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss',
 })
-export class SignUpComponent {}
+export class SignUpComponent implements OnInit {
+
+  protected form!: FormGroup;
+  private formBuilder = inject(FormBuilder);
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm = () => {
+    this.form = this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.maxLength(20)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      email: ['', EMAIL_VALIDATION],
+      password: ['', PASSWORD_VALIDATION]
+    })
+  }
+
+  protected handleNewUser = () => {
+    
+    if (!this.form.valid) {
+      alert("Preencha suas informações corretamente.");
+      this.form.markAllAsTouched();
+      return;
+    };
+    
+    const newUser = this.form.getRawValue();
+    alert(JSON.stringify(newUser));
+  }
+}
