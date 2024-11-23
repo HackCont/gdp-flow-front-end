de675892751c40d62750d7df94140796f2bb209a
thaua-engelmann <engelmann.webdev@gmail.com>
2024-11-23 02:07:49 -0300

    feat(sign-up): criado component para a página e ajustado routing

--- apps/gdp-flow/src/app/app.routes.ts
+++ apps/gdp-flow/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () => import('../app/pages/sign-up/sign-up.component').then(c => c.SignUpComponent)
+    }
+];
