fffb00ab79bf2265fd9e3f2a16d6764361ab521d
rafaelmjardim <rafaelmjardim@hotmail.com>
2024-11-23 20:19:08 -0300

    feat(profile): adicionado header de perfil

--- apps/gdp-flow/src/app/pages/profile/profile.component.ts
+++ apps/gdp-flow/src/app/pages/profile/profile.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterHeaderComponent } from '../../ui/router-header/router-header.component';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterHeaderComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  
+}
