3e0209204eeb29f4d3eca9eef6d1146bf3b28b87
thaua-engelmann <engelmann.webdev@gmail.com>
2024-11-23 19:56:23 -0300

    fix(login): ajustado importação dos módulos de formulário e alterado span para button no texto de *cadastre-se* devido a acessibilidade.

--- apps/gdp-flow/src/app/pages/login/login.component.ts
+++ apps/gdp-flow/src/app/pages/login/login.component.ts
@@ -1,18 +1,16 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthTemplateComponent } from "../../ui/auth-template/auth-template.component";
+import { FORMS_MODULE } from '../../global/modules/forms-module';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatIconModule, AuthTemplateComponent],
+  imports: [CommonModule, MatIconModule, AuthTemplateComponent, FORMS_MODULE],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
