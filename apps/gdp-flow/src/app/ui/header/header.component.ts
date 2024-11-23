116bacfdcffddd612e2a10e3a93e4735984e4b79
rafaelmjardim <rafaelmjardim@hotmail.com>
2024-11-23 16:54:42 -0300

    feat(header) finalizado header

--- apps/gdp-flow/src/app/ui/header/header.component.ts
+++ apps/gdp-flow/src/app/ui/header/header.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { ionLogOutOutline, ionPlayCircleOutline } from '@ng-icons/ionicons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgIcon],
+  providers: [provideIcons({ ionPlayCircleOutline, ionLogOutOutline })],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
