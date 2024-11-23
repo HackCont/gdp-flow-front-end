e34caa8eb960fbf44662c20b1173e7fe0fc49fcb
thaua-engelmann <engelmann.webdev@gmail.com>
2024-11-23 14:03:58 -0300

    fix(appComponent): condicionado grid layout

--- apps/gdp-flow/src/app/app.component.ts
+++ apps/gdp-flow/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { SidebarComponent } from "./ui/sidebar/sidebar.component";
 import { HeaderComponent } from './ui/header/header.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, SidebarComponent, HeaderComponent],
+  imports: [RouterModule, SidebarComponent, HeaderComponent, NgClass],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
