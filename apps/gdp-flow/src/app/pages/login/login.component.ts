9128289363a9f60ac0d04b57865f3c149b6d9f82
thaua-engelmann <engelmann.webdev@gmail.com>
2024-11-23 08:42:29 -0300

    feat(login): criado visual inicial

--- apps/gdp-flow/src/app/pages/login/login.component.ts
+++ apps/gdp-flow/src/app/pages/login/login.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
