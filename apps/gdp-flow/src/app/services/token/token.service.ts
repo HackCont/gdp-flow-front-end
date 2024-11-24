33746ad81e86406d8f953573b992de947debd078
thaua-engelmann <engelmann.webdev@gmail.com>
2024-11-24 01:03:03 -0300

    feat(token): ajustado função de decodeToken para salvar o usuário

--- apps/gdp-flow/src/app/services/token/token.service.ts
+++ apps/gdp-flow/src/app/services/token/token.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { jwtDecode } from "jwt-decode";
 import { CookieService } from 'ngx-cookie-service';
+import { DecodedTokenPropsThatMatter, UserService } from '../user/user.service';
 
 const TOKEN_KEY = 'token';
 
@@ -10,13 +11,22 @@ const TOKEN_KEY = 'token';
 export class TokenService {
 
   private cookieService = inject(CookieService);
+  private userService = inject(UserService);
   
   public saveToken = (token: string) => {
     this.cookieService.set(TOKEN_KEY, token);
+
+    // Decodifica o token.
+    this.decodeToken(token);
   }
-  
+
   private decodeToken = (token: string) => {
-    const decoded = jwtDecode(token);
-    console.log('decoded: ', decoded);
+    const decoded: DecodedTokenPropsThatMatter = jwtDecode(token);
+  
+    // Pega somente as informações mais relevantes para salvar as informações do usuário.
+    const {name, given_name, family_name, email} = decoded;
+    const userData: DecodedTokenPropsThatMatter = {name, given_name, family_name, email};
+
+    this.userService.saveUser(userData);
   }
 }
