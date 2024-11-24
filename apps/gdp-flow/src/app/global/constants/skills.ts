dc58d09eb717cdb00f2149a27ee865847d4e5ad4
thaua-engelmann <engelmann.webdev@gmail.com>
2024-11-24 02:12:47 -0300

    feat(constants): criada lista de skills

---
+++ apps/gdp-flow/src/app/global/constants/skills.ts
@@ -0,0 +1,82 @@
+export const SKILLS: {id: number; name: string}[] = [
+    {
+        id: 1,
+        name: "JavaScript"
+    },
+    {
+        id: 2,
+        name: "TypeScript"
+    },
+    {
+        id: 3,
+        name: "React"
+    },
+    {
+        id: 4,
+        name: "Angular"
+    },
+    {
+        id: 5,
+        name: "Vue.js"
+    },
+    {
+        id: 6,
+        name: "Node.js"
+    },
+    {
+        id: 7,
+        name: "Python"
+    },
+    {
+        id: 8,
+        name: "Django"
+    },
+    {
+        id: 9,
+        name: "Ruby on Rails"
+    },
+    {
+        id: 10,
+        name: "Java"
+    },
+    {
+        id: 11,
+        name: "Spring Boot"
+    },
+    {
+        id: 12,
+        name: "GraphQL"
+    },
+    {
+        id: 13,
+        name: "Docker"
+    },
+    {
+        id: 14,
+        name: "Kubernetes"
+    },
+    {
+        id: 15,
+        name: "AWS"
+    },
+    {
+        id: 16,
+        name: "Git"
+    },
+    {
+        id: 17,
+        name: "SQL"
+    },
+    {
+        id: 18,
+        name: "NoSQL"
+    },
+    {
+        id: 19,
+        name: "Flutter"
+    },
+    {
+        id: 20,
+        name: "Swift"
+    }
+];
