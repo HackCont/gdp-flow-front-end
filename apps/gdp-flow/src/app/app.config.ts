8aa4dfd5f82147d5238df251a3bb3390621c39e0
thaua-engelmann <engelmann.webdev@gmail.com>
2024-11-24 00:16:28 -0300

    feat(app.config): adicionado CookieService como provider

--- apps/gdp-flow/src/app/app.config.ts
+++ apps/gdp-flow/src/app/app.config.ts
@@ -5,6 +5,7 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { provideHttpClient } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,6 +14,8 @@ export const appConfig: ApplicationConfig = {
     provideRouter(appRoutes),
     provideAnimations(),
     provideHttpClient(),
+    
+    CookieService,
 
     {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}}
   ],
