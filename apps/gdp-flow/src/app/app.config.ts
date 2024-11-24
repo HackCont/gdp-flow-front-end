87551d0e7945d802966e63c55cae1191eb3b5a23
thaua-engelmann <engelmann.webdev@gmail.com>
2024-11-23 22:14:06 -0300

    config(app.config): adicionado provider de HttpClient

--- apps/gdp-flow/src/app/app.config.ts
+++ apps/gdp-flow/src/app/app.config.ts
@@ -4,6 +4,7 @@ import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,6 +12,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
     provideAnimations(),
+    provideHttpClient(),
 
     {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}}
   ],
